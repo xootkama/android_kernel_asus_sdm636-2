964abb2571fc6a42efedccee93dd059b40a3bcb8
ajaivasudeve <ajaivasudeve@gmail.com>
2019-09-26 23:18:39 +0530

    power: hack fg_get_battery_temp to report reading from pa_therm0 as battery temperature
    Some X00T devices seems to be having a faulty temperature sensor for the
    battery, reporting temperatures with upto 8 DegC error at times. This leads
    to unnecessary charging throttle, As a workaround for this, hack fg_get_battery_temp
    function to report temperature from pa_therm0 as the battery temperature.
    Report it with an offset of -2 DegC because pa_therm0[VADC_AMUX_THM3_PU2 | 0x4f]
    usually stays at about 2 DegC higher than the battery temperature.
    
    Signed-off-by: ajaivasudeve <ajaivasudeve@gmail.com>

--- drivers/power/supply/qcom/fg-core.h
+++ drivers/power/supply/qcom/fg-core.h
@@ -464,6 +464,8 @@ struct fg_chip {
 	struct work_struct	esr_filter_work;
 	struct alarm		esr_filter_alarm;
 	ktime_t			last_delta_temp_time;
+	struct fg_saved_data	saved_data[POWER_SUPPLY_PROP_MAX];
+	struct qpnp_vadc_chip	*vadc_dev;
 };
 
 /* Debugfs data structures are below */
